9f17105aaaab99bd9a07b38b98a4b056f06a0b4c
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-06-09 18:22:30 +0545

    add signin logic

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 
 exports.signup = async (req, res) => {
   const newuser = new User(req.body);
@@ -7,4 +8,37 @@ exports.signup = async (req, res) => {
   user.salt = undefined;
   user.hashed_password = undefined;
   res.json(user);
+};
+
+exports.signin = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(401).json({
+      error: "User with that email does not exist."
+    });
+  }
+
+  if (!user.authenticate(password)) {
+    return res.status(401).json({
+      error: "Email and password do not match"
+    });
+  }
+
+  const payload = {
+    id: user.id,
+    role: user.role
+  };
+
+  const token = jwt.sign(
+    payload,
+    process.env.JWT_SECRET
+    // {expiresIn:"1h"}
+  );
+
+  res.cookie("t", token, { expire: new Date() + 9999 });
+
+  const { _id, email: mail, name, role } = user;
+  return res.json({ token, user: { _id, mail, name, role } });
 };

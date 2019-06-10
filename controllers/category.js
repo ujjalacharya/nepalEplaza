7c59d1b7d83a8725c75be6de7b6955648b01180a
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-06-10 22:14:05 +0545

    get category by Id

--- controllers/category.js
+++ controllers/category.js
@@ -1,7 +1,21 @@
 const Category = require("../models/Category");
 
+exports.categoryById = async (req, res, next, id) => {
+  const category = await Category.findById(id);
+
+  if (!category) return res.status(400).json({ error: "No category found" });
+
+  req.category = category;
+
+  next();
+};
+
 exports.create = async (req, res) => {
   const newcategory = new Category(req.body);
   const category = await newcategory.save();
   res.json(category);
+};
+
+exports.read = (req, res) => {
+  return res.json(req.category);
 };
--- routes/category.js
+++ routes/category.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
 
-const { create } = require("../controllers/category");
+const { create, read, categoryById } = require("../controllers/category");
 const {requireSignin, isAdmin} = require("../controllers/auth");
 
 router.post("/create", requireSignin, isAdmin, create);
+router.route("/:categoryId").get(read);
+
+router.param("categoryId", categoryById);
 
 module.exports = router;

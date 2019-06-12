7731675b92f61281025406aa86d9c85e63f58456
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-06-12 23:34:11 +0545

    set default authorization to the header

--- client/src/Utils/Requests/Auth.js
+++ client/src/Utils/Requests/Auth.js
@@ -1,12 +1,12 @@
-import appconstants from "../Constants.js";
 import axios from "axios";
+import jwt from "jsonwebtoken";
 
-const { base_url } = appconstants;
+import { API, JWT_SECRET } from "../config";
 
 export const signUp = user => {
   return axios({
     method: "post",
-    url: `${base_url}/signup`,
+    url: `${API}/signup`,
     data: user
   });
 };
@@ -14,7 +14,7 @@ export const signUp = user => {
 export const signIn = user => {
   return axios({
     method: "post",
-    url: `${base_url}/signin`,
+    url: `${API}/signin`,
     data: user
   });
 };
@@ -23,5 +23,29 @@ export const authenticate = (data, next) => {
   if (typeof window !== "undefined") {
     localStorage.setItem("jwt", JSON.stringify(data.data));
     next();
+  }
+};
+
+export const isAuthenticated = () => {
+  if (typeof window == "undefined") {
+    return false;
+  }
+
+  let jsontoken = localStorage.getItem("jwt");
+
+  let data;
+
+  if (jsontoken) {
+    let {token} = JSON.parse(jsontoken);
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      if (err) {
+        data = false;
+      } else {
+        data = JSON.parse(jsontoken);
+      }
+    });
+    return data;
+  } else {
+    return false;
   }
 };

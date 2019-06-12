c79b4dd854dd9d9d2c4fd6796fee0ed811c83ae9
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-06-12 17:53:46 +0545

    add success and error message in signup page

--- client/src/Components/pages/SignIn/index.js
+++ client/src/Components/pages/SignIn/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Layout from "../../core/Layout";
+import Form from "../SignIn/Form";
 
 const SignIn = () => {
-  return <Layout title="SignIn">...</Layout>;
+  return <Layout title="SignIn">{/* <Form /> */}</Layout>;
 };
 
 export default SignIn;
--- client/src/Components/pages/SignUp/index.js
+++ client/src/Components/pages/SignUp/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import Layout from "../../core/Layout";
 import Form from "./Form";
 import { signUp } from "../../../Utils/Requests/Auth";
@@ -12,7 +13,7 @@ const Signup = () => {
     success: false
   });
 
-  const { name, email, password } = state;
+  const { name, email, password, error, success } = state;
 
   const handleChange = e => {
     setState({ ...state, error: false, [e.target.name]: e.target.value });
@@ -20,8 +21,8 @@ const Signup = () => {
 
   const handleSubmit = async e => {
     e.preventDefault();
+    setState({ ...state, error: false });
     const data = await signUp({ name, email, password }).catch(err => {
-      console.log(err.response.data);
       setState({ ...state, error: err.response.data.error });
     });
     if (data)
@@ -34,12 +35,32 @@ const Signup = () => {
       });
   };
 
+  const showError = () => (
+    <div
+      className="alert alert-danger"
+      style={{ display: error ? "" : "none" }}
+    >
+      {error}
+    </div>
+  );
+
+  const showSuccess = () => (
+    <div
+      className="alert alert-info"
+      style={{ display: success ? "" : "none" }}
+    >
+      New account is created. Please <Link to="/signin">Signin</Link>
+    </div>
+  );
+
   return (
     <Layout
       title="Signup"
       description="Signup to nepalEPlaza"
       className="container col-md-8 offset-md-2"
     >
+      {showSuccess()}
+      {showError()}
       <Form
         handleChange={handleChange}
         handleSubmit={handleSubmit}

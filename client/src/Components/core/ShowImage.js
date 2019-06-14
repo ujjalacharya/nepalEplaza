168dd55aea20ad04751b417c41f172afff397c19
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-06-14 18:55:47 +0545

    show image of product

---
+++ client/src/Components/core/ShowImage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { API } from "../../Utils/config";
+
+const ShowImage = ({ item, url }) => (
+  <div className="product-img">
+    <img
+      src={`${API}/${url}/photo/${item.slug}`}
+      alt={item.name}
+      className="mb-3"
+      style={{ height: "450px", objectFit: "cover", width: "100%" }}
+    />
+  </div>
+);
+
+export default ShowImage;

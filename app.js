be5643d541749ffc85a4456ad78aeef3817dff9a
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-06-18 08:36:03 +0545

    heroku setup

--- app.js
+++ app.js
@@ -32,6 +32,14 @@ app.use(function(err, req, res, next) {
   });
 });
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 // Server initialization
 const port = process.env.PORT || 8000;
 
